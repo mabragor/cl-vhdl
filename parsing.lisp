3c0ee21f1089e8a2d936d2eeae0939f0401aa831
Alexandr Popolitov <popolit@gmail.com>
2016-03-21 13:27:21 +0100

    Add part of EBNF rules

--- parsing.lisp
+++ parsing.lisp
@@ -345,6 +345,223 @@
       convolved-binary-string))
       
 
+;;;; OK, let's write all the rules in EBNF, then figure out how to parse this condensed descrption
+;;;; what semantics of all this is
+
+;; We will change this to something meaningful later
+(defmacro define-ebnf-rule (syntax &body body)
+  (declare (ignore syntax body))
+  nil)
 
 
-    
+;;; Design file
+
+(define-ebnf-rule design-file "design-unit { ... }")
+
+;; List of multiple strings is concatenated -- to make linebreaking more convenient
+;; Things introduced in VHDL-2008 begin with underline
+(define-ebnf-rule design-unit ("{ library-clause | use-clause | _context-reference }"
+			       "library-unit"))
+
+;; I should also be able to discern PSL_* rules
+(define-ebnf-rule library-unit ("entity-declaration | architechture-body | package-declaration"
+				"| package-body | package-installation-declaration | configuration-declaration"
+				"| context-declaration | PSL-verification-unit"))
+
+;; CAPS-names denote literal symbols, instead of going down the rules	  
+(define-ebnf-rule library-clause "LIBRARY identifier {, ... } ;")
+
+(define-ebnf-rule context-reference "CONTEXT selected-name {, ... };")
+
+
+;;; Library unit declarations
+
+;; Let's for now stick to convention that grouping brackets of grammar would be (( and ))
+
+;; In general CAPSed names in the beginning denote italicized extra info
+;; (I need to figure out how to actually use it)
+
+(define-ebnf-rule entity-declaration ("ENTITY identifier IS"
+				      "   [ GENERIC ( GENERIC-interface-list ) ; ]"
+				      "   [ PORT ( PORT-interface-list ) ; ]"
+				      "   { entity-declarative-item }"
+				      "[ BEGIN"
+				      "   { concurrent-assertion-statement"
+				      "     | PASSIVE-concurrent-procedure-call-statement"
+				      "     | PASSIVE-process-statement } ]"
+				      "| _PSL-psl-directive" ; I don't know if this line is actually correct
+				      "END [ ENTITY ] [ identifier ] ;"))
+
+(define-ebnf-rule entity-declarative-item ("subprogram-declaration | subprogram-body"
+					   "| subprogram-instantiation-declaration"
+					   "| package-declaration | package-body"
+					   "| package-instantiation-declaration"
+					   "| type-declaration | subtype-declaration"
+					   "| constant-declaration | signal-declaration"
+					   "| SHARED-variable-declaration | file-declaration"
+					   "| alias-declaration | attribute-declaration | attribute-specification"
+					   "| disconnection-specification | use-clause"
+					   "| group-template-declaration | group-declaration"
+					   "| _PSL-property-declaration | _PSL-sequence-declaration"
+					   "| _PSL-clock-declaration"))
+
+(define-ebnf-rule architecture-body ("ARCHITECTURE identifier OF ENTITY-name IS"
+				     "   { block-declarative-item }"
+				     "BEGIN"
+				     "   { concurrent-statement }"
+				     "END [ ARCHITECTURE ] [ identifier ] ;"))
+
+(define-ebnf-rule configuration-declaration ("CONFIGURATION identifier OF ENTITY-name IS"
+					     "   { use-clause | attribute-specification | group-declaration }"
+					     "   { USE VUNIT VERIFICATION-UNIT-name {, ... } ; }"
+					     "   block-configuration"
+					     "END [ CONFIGURATION ] [ identifier ] ;"))
+
+(define-ebnf-rule block-configuration ("FOR (( ARCHITECTURE-name | BLOCK-STATEMENT-label"
+				       "       | GENERATE-STATEMENT-label"
+				       "         [ ( (( STATIC-discrete-range | STATIC-expression"
+				       "                | _ALTERNATIVE-label )) ) ] ))"
+				       "END FOR ;"))
+
+(define-ebnf-rule component-configuration ("FOR component-specification"
+					   "    [ binding-indication ; ]"
+					   "    { USE VUNIT VERIFICATION-UNIT-name {, ... } ; }"
+					   "    [ block-configuration ]"
+					   "END FOR ;"))
+
+(define-ebnf-rule context-declaration ("CONTEXT identifier IS"
+				       "    { library-clause | use-clause | context-reference }"
+				       "END [ CONTEXT ] [ identifier ] ;"))
+
+
+;;; Declarations and Specifications
+
+(define-ebnf-rule package-declaration ("PACKAGE identifier IS"
+				       "    [ GENERIC ( GENERIC-interface-list ) ;"
+				       "      [ GENERIC MAP ( GENERIC-association-list ) ; ] ]"
+				       "    { package-declarative-item }"
+				       "END [ PACKAGE ] [ identifier ] ;"))
+
+(define-ebnf-rule package-declarative-item ("subprogram-declaration | subprogram-instantiation-declaration"
+					    "| package-declaration | package-instantiation-declaration"
+					    "| type-declaration | subtype-declaration"
+					    "| constant-declaration | signal-declaration"
+					    "| variable-declaration | file-declaration"
+					    "| alias-declaration | component-declaration"
+					    "| attribute-declaration | attribute-specification"
+					    "| disconnection-specification | use-clause"
+					    "| group-template-declaration | group-declaration"
+					    "| _PSL-property-declaration | _PSL-sequence-declaration"))
+
+(define-ebnf-rule package-body ("PACKAGE BODY identifier IS"
+				"    { package-body-declarative-item }"
+				"END [ PACKAGE BODY ] [ identifier ] ;"))
+
+(define-ebnf-rule package-body-declarative-item ("subprogram-declaration | subprogram-body"
+						 "| _subprogram-intantiation-declaration"
+						 "| _package-declaration | _package-body"
+						 "| _package-instantiation-declaration"
+						 "| type-declaration | subtype-declaration"
+						 "| constant-declaration | variable-declaration"
+						 "| file-declaration | alias-declaration"
+						 "| _attribute-declaration | _attribute-specification"
+						 "| use-clause | group-template-declaration | group-declaration"))
+
+(define-ebnf-rule _package-instantiation-declaration ("PACKAGE identifier IS NEW UNINSTANTIATED-PACKAGE-name"
+						      "    [ GENERIC MAP ( GENERIC-association-list ) ] ;"))
+
+(define-ebnf-rule subprogram-specification "procedure-specification | function-specification")
+
+(define-ebnf-rule procedure-specification ("PROCEDURE identifier"
+					   "    [ GENERIC ( GENERIC-interface-list )"
+					   "      [ GENERIC MAP ( GENERIC-association-list ) ] ]"
+					   "    [ [ PARAMETER ] ( PARAMETER-interface-list ) ]"))
+
+(define-ebnf-rule function-specification ("[ PURE | IMPURE ] FUNCTION (( identifier | operator-symbol ))"
+					  "    [ GENERIC ( GENERIC-interface-list )"
+					  "      [ GENERIC MAP ( GENERIC-association-list ) ] ]"
+					  "    [ [ PARAMETER ] ( PARAMETER-interface-list ) ] RETURN type-mark"))
+
+(define-ebnf-rule subprogram-declaration "subprogram-specification ;")
+
+(define-ebnf-rule subprogram-body ("subprogram-specification IS"
+				   "    { subprogram-declarative-item }"
+				   "BEGIN { sequential-statement }"
+				   "END [ PROCEDURE | FUNCTION ] [ identifier | operator-symbol ] ;"))
+
+(define-ebnf-rule subprogram-declarative-item ("subprogram-declaration | subprogram-body"
+					       "| _subprogram-instantiation-declaration"
+					       "| _package-declaration | _package-body"
+					       "| _package-instantiation-declaration"
+					       "| type-declaration | subtype-declaration"
+					       "| constant-declaration | variable-declaration"
+					       "| file-declaration | alias-declaration"
+					       "| attribute-declaration | attribute-specification"
+					       "| use-clause | group-template-declaration | group-declaration"))
+
+(define-ebnf-rule _subprogram-instantiation-declaration ("(( PROCEDURE | FUNCTION )) identifier IS"
+							 "    NEW UNINSTANTIATED-SUBPROGRAM-name [ signature ]"
+							 "        [ GENERIC MAP ( GENERIC-association-list ) ] ;"))
+
+(define-ebnf-rule type-declaration ("TYPE identifier IS type-definition ;"
+				    "| TYPE identifier ; "))
+
+(define-ebnf-rule type-definition ("enumeration-type-definition | integer-type-definition"
+				   "| floating-type-definition | physical-type-definition"
+				   "| array-type-definition | record-type-definition"
+				   "| access-type-definition | file-type-definition"
+				   "| protected-type-declaration | protected-type-body"))
+
+(define-ebnf-rule constant-declaration "CONSTANT identifier {, ... } : subtype-indication [ := expression ] ;")
+
+(define-ebnf-rule signal-declaration ("SIGNAL identifier {, ... } : subtype-indication [ REGISTER | BUS ]"
+				      "                             [ := expression ] ;"))
+
+(define-ebnf-rule variable-declaration ("[ SHARED ] VARIABLE identifier {, ... } : subtype-indication"
+					"                                          [ := expression ] ;"))
+
+(define-ebnf-rule file-declaration ("FILE identifier {, ... } : subtype-indication"
+				    "    [ [ OPEN FILE-OPEN-KIND-expression ] IS STRING-expression ] ;"))
+
+(define-ebnf-rule alias-declaration ("ALIAS (( identifier | character-literal | operator-symbol ))"
+				     "    [ : subtype-indication | IS name [ signature ] ;"))
+
+(define-ebnf-rule component-declaration ("COMPONENT identifier [ IS ]"
+					 "    [ GENERIC ( GENERIC-interface-list ) ; ]"
+					 "    [ PORT ( PORT-interface-list ) ; ]"
+					 "END COMPONENT [ identifier ] ;"))
+
+(define-ebnf-rule attribute-declaration "ATTRIBUTE identifier | type-mark ;")
+
+(define-ebnf-rule attribute-specification "ATTRIBUTE identifier OF entity-name-list | entity-class IS expression ;")
+
+(define-ebnf-rule entity-name-list ("(( (( identifier | character-literal | operator-symbol ))[ signature ])){, ...}"
+				    "| OTHERS | ALL"))
+
+(define-ebnf-rule entity-class ("ENTITY | ARCHITECTURE | CONFIGURATION | PACKAGE | PROCEDURE | FUNCTION"
+				"| TYPE | SUBTYPE | CONSTANT | SIGNAL | VARIABLE | FILE | COMPONENT | LABEL"
+				"| LITERAL | UNITS | GROUP | _PROPERTY | _SEQUENCE"))
+
+(define-ebnf-rule configuration-specification ("FOR component-specification binding-indication ;"
+					       "    { USE VUNIT VERIFICATION-UNIT-name {, ... } ; }"
+					       "[ END FOR ; ]"))
+
+(define-ebnf-rule component-specification "(( INSTANTIATION-label {, ...} | OTHERS | ALL )) : COMPONENT-name")
+
+(define-ebnf-rule binding-indication ("USE (( ENTITY ENTITY-name [ ( ARCHITECTURE-identifier ) ]"
+				      "       | CONFIGURATION CONFIGURATION-name | OPEN ))"
+				      "[ GENERIC MAP ( GENERIC-association-list ) ]"
+				      "[ PORT MAP ( PORT-association-list ) ]"))
+
+(define-ebnf-rule disconnection-specification ("DISCONNECT (( SIGNAL-name {, ...} | OTHERS | ALL )) : type-mark"
+					       "    AFTER TIME-expression ;"))
+
+(define-ebnf-rule group-template-declaration "GROUP identifier IS ( (( entity-class [ <> ] )) {, ...} ) ;")
+
+(define-ebnf-rule group-declaration
+  "GROUP identifier : GROUP-TEMPLATE-name ( (( name | character-literal )) {, ...} ) ;")
+
+(define-ebnf-rule use-clause "USE selected-name {, ...} ;")
+
+
+;;; Type Declarations
